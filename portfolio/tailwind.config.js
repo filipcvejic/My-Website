638859b24b46c35bea2f40a830d8b6753f8028b3
Filip Cvejic <97543155+filipcvejic@users.noreply.github.com>
2024-06-29 23:38:35 +0200

    Fixed some tailwind settings bugs

--- portfolio/tailwind.config.js
+++ portfolio/tailwind.config.js
@@ -1,8 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "hero-background": "url('/assets/hero-background.png')",
+        "about-background": "url('/assets/about-background.png')",
+      },
+      colors: {
+        "majorelle-blue": "#6836F4",
+      },
+      fontSize: {
+        sm: "0.8rem",
+        base: "1rem",
+        xl: "1.25rem",
+        "2xl": "1.563rem",
+        "3xl": "1.953rem",
+        "4xl": "2.441rem",
+        "5xl": "3.052rem",
+      },
+      content: {
+        underline: "url('/icons/underline.svg')",
+      },
+    },
   },
   plugins: [],
 };
