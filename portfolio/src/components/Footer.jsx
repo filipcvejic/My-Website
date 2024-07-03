38a5d1b9f04be1e71556aaac39bcc7d8bd605fa2
Filip Cvejic <97543155+filipcvejic@users.noreply.github.com>
2024-07-03 23:45:20 +0200

    Created basic footer

---
+++ portfolio/src/components/Footer.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import MailIcon from "../icons/MailIcon";
+import LinkedInIcon from "../icons/LinkedInIcon";
+import GitHubIcon from "../icons/GitHubIcon";
+import TwitterIcon from "../icons/TwitterIcon";
+
+function Footer() {
+  return (
+    <div className="footer-bg w-full flex flex-col gap-6 justify-center items-center py-16">
+      <div className="flex flex-row gap-5">
+        <MailIcon />
+        <LinkedInIcon />
+        <GitHubIcon />
+        <TwitterIcon />
+      </div>
+      <p className="text-2xl text-white">
+        <a href="mailto:filipinfo@gmail.com">filipinfo@gmail.com</a>
+      </p>
+    </div>
+  );
+}
+
+export default Footer;
