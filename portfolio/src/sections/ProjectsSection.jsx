9331cad4741362ae1cdbca7b9e9f5f9a295aa8c7
Filip Cvejic <97543155+filipcvejic@users.noreply.github.com>
2024-07-11 23:58:34 +0200

    Added new text colors and updated some styles of projects section

--- portfolio/src/sections/ProjectsSection.jsx
+++ portfolio/src/sections/ProjectsSection.jsx
@@ -1,9 +1,17 @@
 import React from "react";
 import ButtonStarsIcon from "../icons/ButtonStarsIcon";
 import Image from "/assets/Untitled.png";
+import Heading from "../components/Heading";
 
 function ProjectsSection() {
   const projects = [
+    {
+      title: "Technomedia",
+      link: "https://technomediashop.netlify.app",
+      description:
+        "with extensive knowledge of the MERN stack. With over a year and a half of experience working on diverse projects, I have honed my skills in both Front-End and Back-End Development.",
+      image: Image,
+    },
     {
       title: "Technomedia",
       link: "https://technomediashop.netlify.app",
@@ -15,29 +23,42 @@ function ProjectsSection() {
 
   return (
     <div className="flex flex-col items-center">
-      <h1 className="text-6xl z-50 relative pt-10 font-bold before:content-underline before:absolute before:left-0 before:top-14 before:-z-10">
-        Projects
-      </h1>
+      <Heading title={`Projects`} />
       <div className="w-full">
-        {projects?.map((project, index) => (
-          <div key={index} className="flex flex-row-reverse gap-7 items-center">
+        {projects?.map((project, index) => {
+          const evenIndex = index % 2 === 0;
+
+          return (
             <div
-              className={`project-bg flex items-center justify-center w-1/2 py-40`}
+              key={index}
+              className={`flex ${
+                evenIndex ? "flex-row-reverse" : ""
+              } gap-7 items-center`}
             >
-              <img src={project.image} className="w-[70%] mr-10" />
-            </div>
-            <div className="w-1/2 pl-36 flex flex-col items-end">
-              <span className="text-7xl">{project.title}</span>
-              <p className="text-end text-2xl">{project.description}</p>
-              <div className="fancy">
-                <button className="overview-project-button flex items-center gap-2 border-majorelle-blue border-2 px-8 py-3 rounded-full text-majorelle-blue font-medium text-lg hover:bg-majorelle-blue hover:text-white">
-                  <ButtonStarsIcon className="mt-5" />
-                  View My Work
-                </button>
+              <div
+                className={`project-bg${
+                  evenIndex ? "1" : "2"
+                } flex items-center justify-center w-1/2 py-40`}
+              >
+                <img src={project.image} className="w-[60%] mr-10" />
+              </div>
+              <div
+                className={`w-1/2 ${
+                  evenIndex ? "pl-36" : "pr-36"
+                } flex flex-col ${evenIndex ? "items-end" : "items-start"}`}
+              >
+                <span className="text-7xl">{project.title}</span>
+                <p className="text-end text-2xl">{project.description}</p>
+                <div className="fancy">
+                  <button className="overview-project-button flex items-center gap-2 border-majorelle-blue border-2 px-8 py-3 rounded-full text-majorelle-blue font-medium text-lg hover:bg-majorelle-blue hover:text-white">
+                    <ButtonStarsIcon className="mt-5" />
+                    View My Work
+                  </button>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
+          );
+        })}
       </div>
     </div>
   );
--- portfolio/tailwind.config.js
+++ portfolio/tailwind.config.js
@@ -6,9 +6,13 @@ export default {
       backgroundImage: {
         "hero-background": "url('/assets/hero-background.png')",
         "about-background": "url('/assets/about-background.png')",
+        "custom-gradient": "rgba(112, 60, 238, 0)",
       },
       colors: {
         "majorelle-blue": "#6836F4",
+        "electric-violet-20": "rgba(109, 54, 241, 0.2)",
+        "electric-violet": "#6D36F1",
+        "blue-bell-20": "rgba(153, 142, 195, 0.2)",
       },
       fontSize: {
         sm: "0.8rem",
