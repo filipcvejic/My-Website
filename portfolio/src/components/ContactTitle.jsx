44c8ac043cf5ffc97fd9b977a62b912c2ac8fe44
Filip Cvejic <97543155+filipcvejic@users.noreply.github.com>
2024-07-15 20:52:16 +0200

    Completed footer responsive

---
+++ portfolio/src/components/ContactTitle.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+function ContactTitle({ color }) {
+  return (
+    <div className="flex flex-col md:gap-8 font-poppins">
+      <div
+        className={`text-${color} font-bold flex flex-col gap-0 leading-none`}
+      >
+        <p className="md-lg:text-6xl md:text-5xl default:text-3xl mb-[-12px]">
+          Let’s
+        </p>
+        <p className="md-lg:text-9xl md:text-8xl default:text-6xl mb-[-12px]">
+          Start
+        </p>
+        <p className="mg-lg:text-7xl md:text-6xl default:text-4xl">
+          a project!
+        </p>
+      </div>
+      <div>
+        <p className="md-lg:text-2xl md:text-xl default:text-lg font-light md:text-black text-white">
+          If you preffer to contact me use this email.
+        </p>
+        <p
+          className={`text-${color} font-bold md-lg:text-3xl md:text-2xl default:text-lg`}
+        >
+          <a href="mailto:filipinfo@gmail.com">filipinfo@gmail.com</a>
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default ContactTitle;
--- portfolio/src/components/Footer.jsx
+++ portfolio/src/components/Footer.jsx
@@ -3,17 +3,21 @@ import MailIcon from "../icons/MailIcon";
 import LinkedInIcon from "../icons/LinkedInIcon";
 import GitHubIcon from "../icons/GitHubIcon";
 import TwitterIcon from "../icons/TwitterIcon";
+import ContactTitle from "./ContactTitle";
 
 function Footer() {
   return (
-    <div className="footer-bg w-full flex flex-col gap-6 justify-center items-center py-16 h-[24vh]">
-      <div className="flex flex-row gap-5">
+    <div className="footer-bg w-full flex md:flex-col gap-6 md:justify-center default:justify-between md:items-center items-end py-2 px-3 md:py-16 md:px-0 md:h-[20vh] default:h-full">
+      <div className="md:hidden">
+        <ContactTitle color={"white"} />
+      </div>
+      <div className="flex md:gap-5 default:flex-col xsm:flex-row default:gap-2">
         <MailIcon />
         <LinkedInIcon />
         <GitHubIcon />
         <TwitterIcon />
       </div>
-      <p className="text-2xl text-white">
+      <p className="text-3xl text-white hover:text-columbia-blue hover:transition-all hover:duration-500 default:hidden md:block">
         <a href="mailto:filipinfo@gmail.com">filipinfo@gmail.com</a>
       </p>
     </div>
