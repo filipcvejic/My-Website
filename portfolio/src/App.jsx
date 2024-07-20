75987a2545a570fbd36183be6ce142f1e9cc1ef9
Filip Cvejic <97543155+filipcvejic@users.noreply.github.com>
2024-07-20 23:53:10 +0200

    Fixed some syntax bugs

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,39 +1,19 @@
-import Footer from "./components/Footer";
 import Header from "./components/Header";
-import SkillsList from "./components/SkillsList";
 import "./index.css";
 import AboutSection from "./sections/AboutSection";
 import ContactSection from "./sections/ContactSection";
-
 import ProjectsSection from "./sections/ProjectsSection";
 
 const App = () => {
   return (
     <div className="relative overflow-x-hidden">
       <Header />
-      <AboutSection />
-      <ProjectsSection />
-      <ContactSection />
+      <div className="flex flex-col">
+        <AboutSection />
+        <ProjectsSection />
+        <ContactSection />
+      </div>
     </div>
-
-    // <div className="relative w-screen h-screen  bg-blue-500 overflow-hidden">
-    //   <Lottie animationData={animationData} />
-    //   {/* <ul className="absolute inset-0 flex items-center justify-center">
-    //     <li>Home</li>
-    //     <li>About</li>
-    //     <li>Projects</li>
-    //     <li>Contact</li>
-    //   </ul> */}
-    //   <div className="absolute inset-36">
-    //     <div className="bg-cover hero-bg flex flex-col col gap-6 justify-center items-center h-full rounded-3xl">
-    //       <h1 className="text-white text-5xl md:text-7xl">Hello I'm Filip</h1>
-    //       <h1>I'm a Full Stack Web Developer</h1>
-    //       <button className="border-majorelle-blue border-2 px-10 py-3 rounded-full text-majorelle-blue font-medium text-lg">
-    //         View My Work
-    //       </button>
-    //     </div>
-    //   </div>
-    // </div>
   );
 };
 
--- portfolio/src/components/Heading.jsx
+++ portfolio/src/components/Heading.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Heading({ title }) {
   return (
-    <h1 className="text-center text-6xl z-50 relative pt-10 font-bold before:content-underline before:absolute before:left-0 before:top-14 before:-z-10">
+    <h1 className="text-center text-6xl z-20 relative pt-12 font-bold before:content-underline before:absolute before:w-full before:left-0 before:top-16 before:-z-10">
       {title}
     </h1>
   );
