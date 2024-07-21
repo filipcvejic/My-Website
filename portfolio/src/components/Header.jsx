2c06c81bd5bcceaa6cf81c4245e0aec7fd30456a
Filip Cvejic <97543155+filipcvejic@users.noreply.github.com>
2024-07-21 23:53:44 +0200

    Updated all sections and header navigation approaches

--- portfolio/src/components/Header.jsx
+++ portfolio/src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
 function Header() {
@@ -10,27 +10,65 @@ function Header() {
   ];
 
   const location = useLocation();
-  const currentHash = location.hash;
+
+  useEffect(() => {
+    const navLinkEls = document.querySelectorAll(".nav-link");
+    const sectionEls = document.querySelectorAll(".section");
+
+    const updateActiveLink = () => {
+      let currentSection = "";
+
+      sectionEls.forEach((sectionEl) => {
+        if (window.scrollY >= sectionEl.offsetTop - 200) {
+          currentSection = sectionEl.id;
+        }
+      });
+
+      const activeClassNames =
+        "text-majorelle-blue after:w-full after:opacity-100";
+
+      navLinkEls.forEach((navLinkEl) => {
+        navLinkEl.classList.remove(...activeClassNames.split(" "));
+
+        if (navLinkEl.href.includes(currentSection)) {
+          navLinkEl.classList.add(...activeClassNames.split(" "));
+        }
+      });
+    };
+
+    const scrollToSection = () => {
+      if (location.hash) {
+        const targetSection = document.querySelector(location.hash);
+        if (targetSection) {
+          targetSection.scrollIntoView({ behavior: "smooth" });
+        }
+      }
+    };
+
+    scrollToSection();
+    updateActiveLink();
+
+    window.addEventListener("scroll", updateActiveLink);
+
+    return () => {
+      window.removeEventListener("scroll", updateActiveLink);
+    };
+  }, [location.hash]);
 
   return (
     <div className="bg-header fixed w-full flex default:px-4 md:px-20 py-2 items-center z-50 shadow-3xl">
       <ul className="list-none flex sm:gap-20 default:justify-around default:text-xl sm:text-2xl font-semibold w-full lg:justify-end">
-        {navigationLinks.map((link, index) => {
-          const isCurrentPage = `#${link.route}` === currentHash;
-
-          return (
-            <li
-              key={index}
-              className={`text-black relative after:content-[''] after:absolute after:left-0 after:bottom-[-5px] after:w-0 after:h-[3px] after:opacity-0 after:transition-all after:duration-300 after:bg-majorelle-blue after:rounded-full${
-                isCurrentPage
-                  ? "text-majorelle-blue after:w-full after:opacity-100"
-                  : ""
-              } hover:text-majorelle-blue hover:after:w-full hover:after:opacity-100`}
+        {navigationLinks.map((link, index) => (
+          <li key={index} className={`text-black`}>
+            <a
+              href={`#${link.route}`}
+              className="nav-link relative after:content-[''] after:absolute after:left-0 after:bottom-[-5px] after:w-0 after:h-[3px] after:opacity-0 after:transition-all after:duration-300 after:bg-majorelle-blue after:rounded-full 
+            hover:text-majorelle-blue hover:after:w-full hover:after:opacity-100"
             >
-              <a href={`#${link.route}`}>{link.label}</a>
-            </li>
-          );
-        })}
+              {link.label}
+            </a>
+          </li>
+        ))}
       </ul>
     </div>
   );
