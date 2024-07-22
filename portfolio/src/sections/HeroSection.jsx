2850eebbca0b0f3e7c45ec627ae592a1057975ab
Filip Cvejic <97543155+filipcvejic@users.noreply.github.com>
2024-07-22 23:55:47 +0200

    Created and implemented connecting dots canvas

--- portfolio/src/sections/HeroSection.jsx
+++ portfolio/src/sections/HeroSection.jsx
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { initCanvasDots } from "../../connecting-dots-animation";
+import leftSpiral from "/assets/spiral-left.png";
+import rightSpiral from "/assets/spiral-right.png";
 
 function HeroSection() {
-  return <div>HeroSection</div>;
+  useEffect(() => {
+    initCanvasDots();
+  }, []);
+
+  return (
+    <div
+      className="section h-screen relative flex justify-center items-center"
+      id="home"
+    >
+      <canvas
+        className="connecting-dots absolute inset-0 w-full h-full z-40"
+        id="canvas"
+      ></canvas>
+      <h1 className="text-8xl tracking-tight flex flex-col items-center font-semibold font-poppins text-center z-10">
+        <span className="text-majorelle-blue tracking-tight">
+          Hello, I'm Filip.
+        </span>
+        <p className="tracking-tight">I'm a Full Stack Web</p>
+        <p>Developer.</p>
+      </h1>
+      <div>
+        <img src={leftSpiral} className="absolute w-[550px] left-0 bottom-0 " />
+        <img
+          src={rightSpiral}
+          className="absolute w-[550px] right-0 bottom-0"
+        />
+      </div>
+    </div>
+  );
 }
 
 export default HeroSection;
