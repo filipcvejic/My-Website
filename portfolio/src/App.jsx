3731fa0219278b2ba73483e2984aedd69e52e7c4
Filip Cvejic <97543155+filipcvejic@users.noreply.github.com>
2024-10-16 22:48:17 +0200

    Added email form functionality

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -4,12 +4,13 @@ import AboutSection from "./sections/AboutSection";
 import ContactSection from "./sections/ContactSection";
 import ProjectsSection from "./sections/ProjectsSection";
 import HeroSection from "./sections/HeroSection";
+import { ToastContainer } from "react-toastify";
 
 const App = () => {
   return (
     <div className="relative overflow-x-hidden font-poppins">
       <Header />
-      <div className="flex flex-col overflow-hidden">
+      <div className="flex flex-col overflow-hidden relative">
         <HeroSection />
         <AboutSection />
         <ProjectsSection />
